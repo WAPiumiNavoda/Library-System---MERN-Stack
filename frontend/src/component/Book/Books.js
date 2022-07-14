b4ab1ed167a8dc28aa60b311cd19877672dfa359
Piumi Navoda <it20617028@my.sliit.lk>
2022-07-14 19:02:32 +0530

    retrive part did

--- frontend/src/component/Book/Books.js
+++ frontend/src/component/Book/Books.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Book from './Book';
+import './Book.css'
 
-function Books() {
+const URL = "http://localhost:8070/books";
+
+
+const fetchHandler = async()=>{
+  return await axios.get(URL).then((res)=>res.data);
+}
+
+
+const Books = () => {
+
+  const [books, setBooks] = useState();
+
+    useEffect(()=>{
+        fetchHandler().then((data)=>setBooks(data.books));
+    },[]);
+    console.log(books);
+
   return (
-    <div>Books</div>
+    <div>
+      <ul>
+        {books && 
+          books.map((book,i)=>(
+
+          <li className='book' key={i}>
+            <Book book={book}/>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
 export default Books
+
